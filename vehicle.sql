9cefe70f0be2039059d4b7188d1479c4d761f0cb
Rahul-Modi <121655268+Rahul-Modi@users.noreply.github.com>
2024-02-11 10:43:50 +0530

    Add files via upload

---
+++ vehicle.sql
@@ -0,0 +1,23 @@
+create database pp;
+use pp;
+create table Two_vehicle
+(
+Two_vehicle_id bigint(100),
+Two_vehicle_name varchar(50)
+);
+
+select *from Two_vehicle;
+create table Three_vehicle
+(
+Three_vehicle_id bigint(100),
+Three_vehicle_name varchar(50)
+);
+select *from Three_vehicle;
+create table Four_vehicle
+(
+Four_vehicle_id bigint(100),
+Four_vehicle_name varchar(50)
+);
+
+select *from Four_vehicle;
+
